8d21219b82349dd1520ea1242694e1dbf743a467
Nagesh-DevTools <nageshk@devtools.in>
2025-07-18 11:07:36 +0530

    Update mongo

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('env.MONGO_URI', {
+mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass:  process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
